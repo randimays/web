3dcd991a2e8074baa89e530417d77f95bd11bcdc
Randi Mays <randimays1024@gmail.com>
2021-11-28 11:11:12 -0600

    Working on the web framework

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { UserForm } from './views/UserForm';
+
+const userForm = new UserForm(
+    document.getElementById('root')
+);
+
+userForm.render();
---
+++ src/views/UserForm.ts
@@ -0,0 +1,30 @@
+export class UserForm {
+    constructor(public parent: Element) {}
+
+    eventsMap() {
+        return {
+            'click:button': this.onButtonClick
+        }
+    }
+
+    onButtonClick(): void {
+        console.log('Hi there');
+    }
+
+    template(): string {
+        return `
+            <div>
+                <h1>User Form</h1>
+                <input />
+                <button>Click Me</button>
+            </div>
+        `;
+    }
+
+    render(): void {
+        const templateElement = document.createElement('template');
+        templateElement.innerHTML = this.template();
+
+        this.parent.append(templateElement.content);
+    }
+}
